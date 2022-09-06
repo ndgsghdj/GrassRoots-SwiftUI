75137f962c1b82708b8dd824de6056ac43e71c01
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-09-06 17:08:15 +0800

    Initial Commit

---
+++ GrassRoots-SwiftUI/GrassRoots_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrassRoots_SwiftUIApp.swift
+//  GrassRoots-SwiftUI
+//
+//  Created by Nikola Winata on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GrassRoots_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

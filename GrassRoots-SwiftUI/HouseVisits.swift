c502f0526c59b5d965b3cea2f8d90c8a94e0c502
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2022-09-10 14:39:23 +0800

    wenyong reports

---
+++ GrassRoots-SwiftUI/HouseVisits.swift
@@ -0,0 +1,20 @@
+//
+//  HouseVisits.swift
+//  GrassRoots-SwiftUI
+//
+//  Created by CHEE WEN YONG stu on 9/9/22.
+//
+
+import SwiftUI
+
+struct HouseVisits: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HouseVisits_Previews: PreviewProvider {
+    static var previews: some View {
+        HouseVisits()
+    }
+}
